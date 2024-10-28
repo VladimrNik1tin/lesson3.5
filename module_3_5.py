f42622a78de04eb310b1354722fb431da8b75454
VladimrNik1tin <nikitin.vladimir99@mail.ru>
2024-10-29 02:56:02 +0400

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    str_number = str_number.replace('o','')
+    str_number = str(str_number.replace('0', ''))
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    return first
+    else:
+        return first
 
-result = get_multiplied_digits(40203)
-print(result)
+
+result1 = get_multiplied_digits(int('40203'))
+print(result1)
